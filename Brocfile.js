d5b90b3a17f30f508824437e4b4f3cbfbeb0ebd1
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2015-03-24 16:01:53 +0100

    update packages

--- Brocfile.js
+++ Brocfile.js
@@ -8,9 +8,8 @@ app.import("bower_components/bootstrap/dist/css/bootstrap.css");
 app.import("bower_components/bootstrap/dist/js/bootstrap.js");
 app.import("bower_components/mopidy.js/dist/mopidy.js");
 app.import("bower_components/levenshtein/lib/levenshtein.js");
-app.import("bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff", {
-  destDir: "fonts"
-});
+app.import("bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff", {destDir: "fonts"});
+app.import("bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2", {destDir: "fonts"});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
