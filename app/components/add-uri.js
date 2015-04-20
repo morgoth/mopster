07c0dacad9f799798eaa6fc154b9a540d5bdff65
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2015-04-20 19:48:46 +0200

    store stream uris in local storage

--- app/components/add-uri.js
+++ app/components/add-uri.js
@@ -5,9 +5,35 @@ export default Ember.Component.extend({
   layout: layout,
   classNames: ["pull-left"],
 
+  setup: function () {
+    this.set("streamUris", JSON.parse(localStorage.getItem("streamUris")));
+  }.on("init"),
+
   actions: {
-    add: function() {
-      this.get("mop").addUris([this.get("url")]).then( () => {
+    add: function () {
+      this.send("addUri", this.get("url"));
+      this.set("url", null);
+    },
+
+    remove: function (uri) {
+      let items = this.get("streamUris").without(uri);
+      this.set("streamUris", items);
+      localStorage.setItem("streamUris", JSON.stringify(items));
+    },
+
+    addUri: function (uri) {
+      this.get("mop").addUris([uri]).then( () => {
+        let items = JSON.parse(localStorage.getItem("streamUris"));
+
+        if (Ember.isArray(items)) {
+          items.push(uri);
+        } else {
+          items = [uri];
+        }
+        items = items.uniq();
+        this.set("streamUris", items);
+        localStorage.setItem("streamUris", JSON.stringify(items));
+
         this.$("#add-url").modal("hide");
       });
     }
