2b4071c310adb24266c2d7d7c07e86c16155c9ac
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2015-08-16 18:16:06 +0200

    Remove size fetching as it does not make any visible difference

--- app/components/album-art.js
+++ app/components/album-art.js
@@ -5,22 +5,15 @@ export default Ember.Component.extend({
   layout: layout,
   classNames: ["album-art"],
 
-  getSize: function() {
-    this.set("size", $("img", this.element).width());
-  }.on("didInsertElement"),
-
   getImage: function() {
-    var album = this.get("album");
-    if ("images" in album && album.images.length >= 1) {
-      this.set("imageSource", album.images[0]);
+    let album = this.get("album");
+    if ("images" in album && album.images.length) {
+      this.set("imageURL", album.images[0]);
     } else {
       $.getJSON(`http://itunes.apple.com/search?term=${encodeURIComponent(album.name)}&limit=1&entity=album&callback=?`)
-        .done((e) => {
-          this.set("imageSource", e.results[0].artworkUrl100.replace("100x100", `${this.size}x${this.size}`))
-          this.set("loading", false)
-        })
-        .fail((e) => { console.error(e); this.set("loading", false); });
-      this.set("loading", true)
+      .done((response) => {
+        this.set("imageURL", response.results[0].artworkUrl100);
+      });
     }
-  }.on("didInsertElement")
+  }.on("init")
 });
