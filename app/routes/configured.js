f4ce53661b5b8328fc5e31a3a45cf29e67f12bb6
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2021-05-17 17:39:50 +0200

    Fix app

--- app/routes/configured.js
+++ app/routes/configured.js
@@ -2,6 +2,7 @@ import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
 
 export default class ConfiguredRoute extends Route {
+  @service router;
   @service mopidyClient;
   @service player;
 
